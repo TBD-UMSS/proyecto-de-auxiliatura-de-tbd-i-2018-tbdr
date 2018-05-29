2512951b66339dd186c96390ee792306a1fb8456
roxanarocio <38479419+roxanarocio@users.noreply.github.com>
2018-05-29 05:18:44 +0200

    Update dependence0001.sql
    few changes made

--- sistema_curriculum/dependence0001.sql
+++ sistema_curriculum/dependence0001.sql
@@ -11,7 +11,7 @@ ALTER TABLE curr.estudio
 ALTER TABLE curr.estudio
    ADD CONSTRAINT estudio_fk FOREIGN KEY (id_institucion_educativa)
      REFERENCES curr.institucion_educativa(id_institucion_educativa)
-     ON DELETE CASCADE
+     ON DELETE NO ACTION
      ON UPDATE NO ACTION
      NOT DEFERRABLE;
 
@@ -23,7 +23,7 @@ ALTER TABLE curr.experiencia
      NOT DEFERRABLE;
  
 ALTER TABLE curr.experiencia
-   ADD CONSTRAINT experiencia_fk FOREIGN KEY (id_empresa)
+   ADD CONSTRAINT experiencia_fk1 FOREIGN KEY (id_empresa)
      REFERENCES curr.empresa (id_empresa)
      ON DELETE CASCADE
      ON UPDATE NO ACTION
@@ -33,14 +33,14 @@ ALTER TABLE curr.experiencia
 ALTER TABLE curr.certificado
    ADD CONSTRAINT certificado_fk FOREIGN KEY (id_dato_personal)
    REFERENCES curr.dato_personal(id_dato_personal)
-   ON DELETE CASCADE
+   ON DELETE NO ACTION
    ON UPDATE NO ACTION
    NOT DEFERRABLE;
 
 ALTER TABLE curr.tipo_certificado
    ADD CONSTRAINT tipo_certificado_fk FOREIGN KEY (id_certificado)
    REFERENCES curr.certificado(id_certificado)
-   ON DELETE CASCADE
+   ON DELETE NO ACTION
    ON UPDATE NO ACTION
    NOT DEFERRABLE;
 
